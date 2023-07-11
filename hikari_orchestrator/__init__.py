d66c0644d470ccda62917b7383d2caf76549dc03
Faster Speeding <luke@lmbyrne.dev>
2023-07-11 18:26:48 +0100

    Add proper description

--- hikari_orchestrator/__init__.py
+++ hikari_orchestrator/__init__.py
@@ -28,7 +28,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""Temp"""
+"""System for managing hikari shards across processes/systems."""
 from __future__ import annotations
 
 __all__ = ["run_server", "run_subprocesses", "spawn_subprocesses"]
